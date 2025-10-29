fa3f8ae772878eb33bf7c7b91720ec5ed5e59a33
Andrei <andreihoszu2@yahoo.ro>
2025-10-29 20:38:01 +0200

    master: added assimp library

--- Source/ResourceManager/Types/ModelFileManager.cpp
+++ Source/ResourceManager/Types/ModelFileManager.cpp
@@ -1,6 +1,10 @@
 #pragma once
 #include "ModelFileManager.hpp"
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 ModelFileManager::ModelFileManager()
 {
 }
