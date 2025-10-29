2a0718338553d29129b73d5ad4b6526f1ffd19ab
Andrei <andreihoszu2@yahoo.ro>
2025-10-29 20:27:28 +0200

    master: added stb and openal libraries

--- Source/ResourceManager/Types/AudioFileManager.hpp
+++ Source/ResourceManager/Types/AudioFileManager.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "../BaseFileManager/BaseFileManager.hpp"
 
+#include "AL/al.h"
+#include "AL/alc.h"
+
 class AudioFileManager : BaseFileManager {
 public:
 	AudioFileManager();
